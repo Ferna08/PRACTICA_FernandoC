f9e99ef21812a98c0d1e76fccab61b1bfc8149aa
SrFerna <158377197+SrFerna@users.noreply.github.com>
2025-04-09 19:02:56 -0600

    Creación

---
+++ Prac5_Fernando/src/main/java/practica5_FernandoC/Pract5/Pract5Application.java
@@ -0,0 +1,13 @@
+package practica5_FernandoC.Pract5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pract5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pract5Application.class, args);
+	}
+
+}
---
+++ Prac5_Fernando/src/test/java/practica5_FernandoC/Pract5/Pract5ApplicationTests.java
@@ -0,0 +1,13 @@
+package practica5_FernandoC.Pract5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
